6175a12a264f37046af846b1a6bce1403ffea8e1
jesusalvarado2023 <132959180+jesusalvarado2023@users.noreply.github.com>
2025-07-31 19:58:51 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from clases import clase1, clase2, clase3
+
+st.sidebar.title("Clases de OpenCV")
+opcion = st.sidebar.radio("Selecciona una clase:", ("Clase 1", "Clase 2", "Clase 3"))
+
+if opcion == "Clase 1":
+    clase1.mostrar()
+elif opcion == "Clase 2":
+    clase2.mostrar()
+elif opcion == "Clase 3":
+    clase3.mostrar()
+
