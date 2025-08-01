78e4c4fcb2c1e075e8004b5dad8c377880ffef51
jesusalvarado2023 <132959180+jesusalvarado2023@users.noreply.github.com>
2025-08-01 01:59:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ from clases import clase1, clase2, clase3
 st.sidebar.title("Clases de OpenCV")
 st.sidebar.info("Dr. Jesus Alvarado-Huayhuaz")
 
-opcion = st.sidebar.radio("Selecciona una clase:", ("Clase 1", "Clase 2", "Clase 3"))
+opcion = st.sidebar.radio("Selecciona una clase:", ("Ejemplo 1", "Ejemplo 2", "Ejemplo 3"))
 
-if opcion == "Clase 1":
+if opcion == "Ejemplo 1":
     clase1.mostrar()
-elif opcion == "Clase 2":
+elif opcion == "Ejemplo 2":
     clase2.mostrar()
-elif opcion == "Clase 3":
+elif opcion == "Ejemplo 3":
     clase3.mostrar()
 
