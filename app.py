82668d3cb11968a35b045382131a0720d351843b
jesusalvarado2023 <132959180+jesusalvarado2023@users.noreply.github.com>
2025-08-01 01:58:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 from clases import clase1, clase2, clase3
 
 st.sidebar.title("Clases de OpenCV")
+st.sidebar.info("Dr. Jesus Alvarado-Huayhuaz")
+
 opcion = st.sidebar.radio("Selecciona una clase:", ("Clase 1", "Clase 2", "Clase 3"))
 
 if opcion == "Clase 1":
