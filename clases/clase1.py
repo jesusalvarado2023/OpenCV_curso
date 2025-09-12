165d85d48a94c66915cdc74f234ea15650b40bfc
jesusalvarado2023 <132959180+jesusalvarado2023@users.noreply.github.com>
2025-09-11 20:06:34 -0500

    Update clase1.py

--- clases/clase1.py
+++ clases/clase1.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import cv2
-from PIL import Image
 import numpy as np
+from PIL import Image
 
 def mostrar():
     st.title("Ejemplo 1: Cargar y Mostrar Imágenes")
@@ -20,7 +20,17 @@ cv2.destroyAllWindows()
 
     st.write("Resultado en Streamlit:")
 
-    imagen = cv2.imread('imagenes/minion.png')
+    # Subida de imagen
+    archivo = st.file_uploader("Sube una imagen", type=["jpg", "jpeg", "png"])
+
+    if archivo is not None:
+        # Leer la imagen subida
+        file_bytes = np.asarray(bytearray(archivo.read()), dtype=np.uint8)
+        imagen = cv2.imdecode(file_bytes, 1)
+    else:
+        # Imagen por defecto
+        imagen = cv2.imread('imagenes/minion.png')
+
+    # Convertir a RGB para mostrar en Streamlit
     imagen_rgb = cv2.cvtColor(imagen, cv2.COLOR_BGR2RGB)
-    #st.image(imagen_rgb, caption="Imagen cargada", use_column_width=True)
     st.image(imagen_rgb, caption="Imagen cargada", use_container_width=True)
