d2e9de7e039bbdf51c249e49366e4f8170216f63
jesusalvarado2023 <132959180+jesusalvarado2023@users.noreply.github.com>
2025-12-02 17:37:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import streamlit as st
-#from clases import clase1, clase2, clase3
+from clases import clase1, clase2, clase3
 
 st.sidebar.title("Clases de OpenCV")
 st.sidebar.info("Dr. Jesus Alvarado-Huayhuaz")
 
-#opcion = st.sidebar.radio("Selecciona una clase:", ("Ejemplo 1", "Ejemplo 2", "Ejemplo 3"))
+opcion = st.sidebar.radio("Selecciona una clase:", ("Ejemplo 1", "Ejemplo 2", "Ejemplo 3"))
 
-#if opcion == "Ejemplo 1":
-    #clase1.mostrar()
-#elif opcion == "Ejemplo 2":
+if opcion == "Ejemplo 1":
+    clase1.mostrar()
+elif opcion == "Ejemplo 2":
     #clase2.mostrar()
-#elif opcion == "Ejemplo 3":
+elif opcion == "Ejemplo 3":
     #clase3.mostrar()
 
