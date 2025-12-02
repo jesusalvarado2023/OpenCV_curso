0f8c868088827f0be7b2b179c694d470127f0408
jesusalvarado2023 <132959180+jesusalvarado2023@users.noreply.github.com>
2025-12-02 17:31:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ st.sidebar.info("Dr. Jesus Alvarado-Huayhuaz")
 opcion = st.sidebar.radio("Selecciona una clase:", ("Ejemplo 1", "Ejemplo 2", "Ejemplo 3"))
 
 if opcion == "Ejemplo 1":
-    clase1.mostrar()
+    #clase1.mostrar()
 elif opcion == "Ejemplo 2":
-    clase2.mostrar()
+    #clase2.mostrar()
 elif opcion == "Ejemplo 3":
-    clase3.mostrar()
+    #clase3.mostrar()
 
